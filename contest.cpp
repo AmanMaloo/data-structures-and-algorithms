a39a965cbf6e269d3a01a9c1e9b719df49cffc12
AmanMaloo <168951383+AmanMaloo@users.noreply.github.com>
2024-12-03 20:30:07 +0530

    Create contest.cpp

---
+++ contest.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define vl vector<ll>
+#define f(i,n) for(ll i = 0 ; i < n ; i++)
+#define rf(i,n) for(ll i = n-1;i>=0;i--)
+#define sort(arr) sort(arr.begin(),arr.end())
+#define rsort(arr)sort(arr.rbegin(),arr.rend())
+#define rev(arr) reverse(arr.begin(),arr.end())
+#define vi vector<int>
+int main()
+{
+    int t;
+    while(t--){
+        
+    }
+
+    return 0;
+}
