479ddc6db92afa877e2bcdc9ba151c364d1a9189
AmanMaloo <168951383+AmanMaloo@users.noreply.github.com>
2025-01-07 15:53:38 +0530

    Update contest.cpp

--- contest.cpp
+++ contest.cpp
@@ -1,4 +1,7 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+typedef __gnu_pbds::tree<int, __gnu_pbds::null_type, less<int>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update> ordered_set;
 using namespace std;
 #define ll long long
 #define vl vector<ll>
