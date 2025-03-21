04c47b95b62112d147594e4f61394a31f07e69ff
AmanMaloo <168951383+AmanMaloo@users.noreply.github.com>
2025-03-21 18:29:04 +0530

    Update contest.cpp

--- contest.cpp
+++ contest.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-typedef __gnu_pbds::tree<int, __gnu_pbds::null_type, less<int>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update> ordered_set;
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+//typedef __gnu_pbds::tree<int, __gnu_pbds::null_type, less<int>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update> ordered_set;
 #define int long long
 #define f(i,n) for(int i = 0 ; i < n ; i++)
 #define rf(i,n) for(int i = n-1;i>=0;i--)
