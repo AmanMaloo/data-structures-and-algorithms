4c2759d1d7584457ebb333844932348872ff7577
AmanMaloo <168951383+AmanMaloo@users.noreply.github.com>
2025-11-24 16:43:51 +0530

    adding point update
    modify functions according to requirements

--- Segement Tree.cpp
+++ Segement Tree.cpp
@@ -17,3 +17,13 @@ int query(int ind,int low,int high,int l,int r){
     int right = query(2*ind+2,mid+1,high,l,r);
     return min(left,right);
 }
+void update(int ind , int low , int high , int i , int x){
+    if(low == high){
+        seg[ind] = add(seg[ind],x);
+        return;
+    }
+    int mid = (low+high)/2;
+    if(mid >= i) update(2*ind+1,low,mid,i,x);
+    else update(2*ind+2,mid+1,high,i,x);
+    seg[ind] = add(seg[2*ind+1],seg[2*ind+2]);
+}
